da682e5ef187336caea8aea7370684ef5d7ef375
Ervin1809 <mervink1809@gmail.com>
2024-04-18 17:48:25 +0700

    Kumpul Tugas 4 (Encapsulation)

---
+++ H071231050/Tugas Praktikum 4/login_app/models/Profile.java
@@ -0,0 +1,44 @@
+package login_app.models;
+
+public class Profile {
+    private String nickname;
+    private String name;
+    private int age;
+    private String hobby;
+    
+    public Profile() {
+    
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+}
