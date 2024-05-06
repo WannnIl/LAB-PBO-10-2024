6a31d2b6c7b77e7ab16a2f45f3622f374e22196c
Ervin1809 <mervink1809@gmail.com>
2024-05-06 17:13:07 +0800

    Kumpul Tugas 5 (Inheritance)

---
+++ H071231050/Tugas Praktikum 5/Tugas5No1/Models/Attirbut.java
@@ -0,0 +1,12 @@
+package TugasPraktikum5.Tugas5No1.Models;
+
+public class Attirbut {
+    final double PI = Math.PI;
+    int tinggi;
+    int panjang;
+    int lebar;
+    int jari2;
+    int sisi;
+    int rusuk;
+    
+}
